cf18253cb7256b5593f5a748a78ebc1b873c8e03
laurakomp <82443899+laurakomp@users.noreply.github.com>
2021-05-25 09:37:41 +0300

    Downloaded Axios and API

---
+++ src/app.js
@@ -0,0 +1,15 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#weather-description");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
+let apiKey = "ebde765ff48c731ae66ed15747f18ce4";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
