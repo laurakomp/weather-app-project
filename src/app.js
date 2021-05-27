22d0ca206676cb73313ece95c2715f30a9157c28
laurakomp <82443899+laurakomp@users.noreply.github.com>
2021-05-27 17:58:14 +0300

    Showing current temp, wind, humidity

--- src/app.js
+++ src/app.js
@@ -3,10 +3,14 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#weather-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "ebde765ff48c731ae66ed15747f18ce4";
