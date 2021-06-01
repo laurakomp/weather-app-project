aa25edafcf881bd0d482679ada68ae43d044f76a
laurakomp <82443899+laurakomp@users.noreply.github.com>
2021-06-01 10:08:48 +0300

    Added weather icons, made bigger changes in layout and design

--- src/app.js
+++ src/app.js
@@ -30,10 +30,7 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
+  iconElement.setAttribute("src", `icons/${response.data.weather[0].icon}.png`);
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
