d5c711b70aeb555eeaa9a47a4069b565350590ee
laurakomp <82443899+laurakomp@users.noreply.github.com>
2021-06-10 19:38:10 +0300

    Added JS for forecast

--- src/app.js
+++ src/app.js
@@ -13,6 +13,30 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      ` 
+    <div class="col-2">
+      <div class="weather-forecast-date"> ${day} </div>
+      <img src="file:///Users/laurakomp/Desktop/weather-app-final/icons/03d.png" alt="" width="40px">
+      <div class="weather-forecast-temperature"> 
+        <span class="weather-forecast-temperature-max">18°</span>
+        <span class="weather-forecast-temperature-min">12°</span>
+      </div>      
+    </div>
+    `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -75,3 +99,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Tallinn");
+
+displayForecast();
