672164ac8bbce3d5d92bd744dcc2c4f667f7b9ac
laurakomp <82443899+laurakomp@users.noreply.github.com>
2021-06-13 10:42:39 +0300

    Small css improvements, updated fonts and icons

--- src/app.js
+++ src/app.js
@@ -61,7 +61,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "ebde765ff48c731ae66ed15747f18ce4";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
